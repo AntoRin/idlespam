0530663e1dcf04e49cf1fba1fdcd03d60745e1aa
Anto Rinish <AntoRinishGanth.A@lntinfotech.com>
2023-02-22 17:58:41 +0530

    Add pyautogui to keep track of mouse movements - to prevent spam when user is already working

--- main.py
+++ main.py
@@ -2,27 +2,46 @@ import ctypes
 from threading import Thread
 from time import sleep
 import sys
+import pyautogui
 
 
 class PreventIdle:
     def __init__(self):
         options = self.__parse_commandline_args()
 
-        self.interval = float(options.get(
-            "i") or options.get("interval") or 30)
+        self.interval = float(options.get("i") or options.get("interval") or 30)
         self.key = int(options.get("k") or options.get("key") or "0x14", 16)
+        self.manual_mode = options.get("noauto")
+        
+        print(options)
 
         self.__input_thread = None
         self.__process_thread = None
+
+        self.__saved_cursor_pos = None
 
     def __parse_commandline_args(self):
         arguments = sys.argv
         options = {}
         for idx, arg in enumerate(arguments):
-            if arg.startswith("--") and arg.find("=") != -1:
-                pair = arg.split("=")
-                options[pair[0].replace("-", "")] = pair[1]
-            elif arg.startswith("-") and not arg.startswith("--") and idx + 1 < len(arguments) and not arguments[idx + 1].startswith("-"):
+            if arg.startswith("--"):
+                if arg.find("=") != -1:
+                    pair = arg.split("=")
+                    options[pair[0].replace("-", "")] = pair[1]
+                else:
+                    if idx + 1 < len(arguments) and not arguments[idx + 1].startswith(
+                        "-"
+                    ):
+                        options[arg.replace("-", "")] = arguments[idx + 1]
+                    if idx + 1 >= len(arguments) or arguments[idx + 1].startswith("-"):
+                        options[arg.replace("-", "")] = True
+
+            elif (
+                arg.startswith("-")
+                and not arg.startswith("--")
+                and idx + 1 < len(arguments)
+                and not arguments[idx + 1].startswith("-")
+            ):
                 options[arg.replace("-", "")] = arguments[idx + 1]
         return options
 
@@ -38,7 +57,8 @@ class PreventIdle:
             self.__input_thread = Thread(target=self.get_input, daemon=True)
             self.__input_thread.start()
             self.__process_thread = Thread(
-                target=self.press_key_intermittently, daemon=True)
+                target=self.press_key_intermittently, daemon=True
+            )
             self.__process_thread.start()
             self.__input_thread.join()
         except KeyboardInterrupt:
@@ -48,10 +68,25 @@ class PreventIdle:
         try:
             while True:
                 sleep(self.interval)
-                ctypes.windll.user32.keybd_event(self.key, 0, 0, 0)
-                ctypes.windll.user32.keybd_event(self.key, 0, 0x0002, 0)
+                
+                if self.manual_mode:
+                    self.__perform_key_press(self.key)
+                    continue
+
+                curr_cursor_pos = pyautogui.position()
+                
+                if curr_cursor_pos == self.__saved_cursor_pos:
+                    self.__perform_key_press(self.key)
+                    continue
+
+                self.__saved_cursor_pos = curr_cursor_pos
+
         except Exception as e:
             return
+        
+    def __perform_key_press(self, key):
+        ctypes.windll.user32.keybd_event(key, 0, 0, 0)
+        ctypes.windll.user32.keybd_event(key, 0, 0x0002, 0)
 
 
 prevent_idle_handler = PreventIdle()
